796258fe97c13bfa8f234559a2bc67795ebf53dd
Matthew Sun <matthewsun98@gmail.com>
2021-12-05 13:40:08 -0500

    Correct centering issues, add ability to add new nodes

--- cdhl/src/App.js
+++ cdhl/src/App.js
@@ -3,37 +3,69 @@ import './App.css';
 import Cytoscape from 'cytoscape';
 import dagre from 'cytoscape-dagre';
 import CytoscapeComponent from 'react-cytoscapejs';
+import React from 'react';
 
 Cytoscape.use(dagre);
 
-function App() {
-  const elements = [
-    { data: { id: 'one', label: 'Node 1' }, position: { x: 0, y: 0 } },
-    { data: { id: 'two', label: 'Node 2' }, position: { x: 100, y: 0 } },
-    { data: { source: 'one', target: 'two', label: 'Edge from Node1 to Node2' } }
-  ];
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      elements: [
+        { data: { id: 'one', label: 'Universal health care' } },
+        { data: { id: 'two', label: 'Raise taxes' } },
+        { data: { id: 'three', label: 'M4A' } },
+        { data: { source: 'one', target: 'two', label: 'Edge from Node1 to Node2' } },
+        { data: { source: 'one', target: 'three', label: 'Edge from Node1 to Node3' } }
+      ],
+      numNodes: 3
+    }
+    this.addChild = this.addChild.bind(this);
+  }
 
-  const layout = { name: 'dagre' };
-  const style = {width:'800px', height: '500px'};
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          YAY everything works now!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <CytoscapeComponent elements={elements} layout={layout} style={style}/>
-    </div>
-  );
+  addChild(){
+    // find selected element
+    let selected = this.cy.nodes(':selected');
+    if(selected.length > 0){
+      let selectedNode = selected[0].data().id;
+      this.setState((state) => {
+        let total = state.numNodes + 1;
+        let newId = '' + total;
+        let newNode = {data: {id: newId, label: 'Node ' + newId}};
+        let newEdge = {data: {source: selectedNode, target: newId}};
+        return {
+          elements: [...state.elements, newNode, newEdge],
+          numNodes: total,
+        }
+      });
+    }
+  }
+
+  componentDidMount() {
+    console.log("yay");
+    this.cy.resize();
+  }
+
+  render() {
+    const layout = { name: 'dagre', fit: true, padding: 90 };
+    const style = {width:'800px', height: '500px'};
+    return (
+      <div className="App">
+        <CytoscapeComponent
+          cy={(cy) => {
+            cy.on('add', 'node', _evt => {
+                cy.layout(layout).run()
+            });
+            this.cy = cy;
+          }}
+          elements={this.state.elements}
+          layout={layout}
+          style={style}
+        />
+        <button onClick={this.addChild}>Add Child</button>
+      </div>
+    );
+  }
 }
 
 export default App;
